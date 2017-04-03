1730ad244b47a4e7ed31d935b4b6ee502c04a3f9
ChauhanSiddharth <siddharth.chauhan@gmail.com>
2017-04-03 20:25:27 +0530

    Instructor Course layout changed

--- class_details/views.py
+++ class_details/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Class_Member,Class_Master
 from .form import Class_MemberForm,ClassForm
+from course.models import material
 from course.models import course_details,instructor_course
 
 # Create your views here.
@@ -12,7 +13,13 @@ def View_Class(request):
         class_details = Class_Master.objects.filter( Instructor_id = instructor )
         classno = class_details.values()[0]['class_id']
         class_member = Class_Member.objects.filter( class_member_id = classno )
-        return render(request,'class_details.html',{'data':class_details,'member':class_member})
+        course = instructor_course.objects.filter(instructor_id=user.id)
+        try:
+            course_id = course.values()[0]['id']
+            materials = material.objects.filter(course_id_id=course_id)
+        except Exception:
+            return render(request,'class_details.html',{'data':class_details,'member':class_member})
+        return render(request, 'class_details.html', {'data': class_details, 'member': class_member,'material':materials})
 
 def Add_Class(request):
     if request.user.is_authenticated():
